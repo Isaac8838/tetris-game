31eafb86793c38fedb82dbcf1923f07f0f2cae7b
manganese <chinoyoshino@gmail.com>
2024-05-05 22:55:56 +0800

    delete

---
+++ frontend/tetris-game/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {Component} from 'react';
+
+export default class App extends Component{
+  render(){
+    return(
+      <div>
+
+      </div>
+    )
+  }
+}
