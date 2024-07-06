dfca2b4424336fb00e36bb45279869e03abcded5
邱得恩 <qiudeen@qiudeendebijixingdiannao.local>
2024-07-06 23:06:39 +0800

    BASE_URL

--- frontend/tetris-game/src/WebAPI.jsx
+++ frontend/tetris-game/src/WebAPI.jsx
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:8080";
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 if (BASE_URL === undefined) {
     window.location.href = "/error.html";
