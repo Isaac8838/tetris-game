1ca6bed50c7e814efdca42d3aea0e82e878a0669
邱得恩 <qiudeen@qiudeendebijixingdiannao.local>
2024-10-08 16:37:52 +0800

    websoket

--- frontend/tetris-game/src/pages/Room.jsx
+++ frontend/tetris-game/src/pages/Room.jsx
@@ -7,6 +7,8 @@
 //     TableRow,
 // } from "@/components/ui/table";
 
+import useWebSocket from "react-use-websocket";
+
 import Table from "@/ui/Table";
 
 const room = [
@@ -17,20 +19,29 @@ const room = [
     { roomId: 26, hadKey: false, roomName: "no friend", host: "kodoku" },
 ];
 
+const socketUrl = "ws://localhost:8081/lobby";
+const testUrl =
+    "wss://demo.piesocket.com/v3/channel_123?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self";
+
 const Room = () => {
+    useWebSocket(socketUrl, {
+        onOpen: () => console.log("websocket connected"),
+        onClose: () => console.log("websocket disconnected"),
+    });
+
     return (
-        <Table cols="0.2fr 0.5fr 1fr 0.5fr 0.5fr">
-            <Table.Container>
-                <Table.Header
-                    titles={["", "Room ID", "Room Name", "Host", ""]}
-                />
-                <div className="flex flex-col gap-1 overflow-hidden rounded-2xl">
-                    {room.map((data, i) => (
-                        <Table.RoomBody key={`room-${i}`} room={data} />
-                    ))}
-                </div>
-            </Table.Container>
-        </Table>
+        <div className="m-auto flex h-full w-[70%] max-w-[900px] flex-col items-center justify-center">
+            <Table cols=" 0.5fr 1fr 0.5fr">
+                <Table.Container>
+                    <Table.Header titles={["Room ID", "Room Name", "Host"]} />
+                    <div className="flex flex-col gap-1 overflow-hidden rounded-2xl">
+                        {room.map((data, i) => (
+                            <Table.RoomBody key={`room-${i}`} room={data} />
+                        ))}
+                    </div>
+                </Table.Container>
+            </Table>
+        </div>
     );
 };
 
